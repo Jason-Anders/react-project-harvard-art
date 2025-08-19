5be128f06d31ac7a56a1043de5ed1cb442c8f3df
Jason-Anders <jason.charles.anders@gmail.com>
2025-08-19 15:45:11 -0400

    added home route and page not found route

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import hardvardArt from './data/harvardArt';
 import GalleryNavigation from './components/GalleryNavigation';
 import GalleryView from './components/GalleryView';
-import {Route} from 'react-router-dom';
+import {Route, Switch} from 'react-router-dom';
 
 function App() {
   // console.log(hardvardArt);
   return (
     <div className='page-wrapper'>
       <GalleryNavigation galleries={hardvardArt.records} />
+      <Switch>
+      <Route exact path='/'>
+        <h2>Hardvard Art Museum</h2>
+        <p>Look, but Don't Touch. Please select a Gallery in the navigation bar.</p>
+      </Route>
+
       <Route path='/galleries/:galleryId'>
         <GalleryView galleries={hardvardArt.records}/>
       </Route>
+      <Route>
+        <h2>Page Not Found</h2>
+      </Route>
+      </Switch>
     </div>
   );
 }
