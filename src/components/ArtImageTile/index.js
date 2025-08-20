4e652458305006e13cdd98ac081ff6df9e9232e3
Jason-Anders <jason.charles.anders@gmail.com>
2025-08-20 13:12:35 -0400

    array of pics per gallery with specific links for each art piece

--- src/components/ArtImageTile/index.js
+++ src/components/ArtImageTile/index.js
@@ -1,7 +1,7 @@
 import {Link} from 'react-router-dom';
 
 const ArtImageTile = ({art, gallery})=>{
-console.log(art[0]);
+// console.log(art[0]);
     return(
             <Link to={`/galleries/${gallery.id}/art/${art[0].imageid}`}>
                 <img src={art[0].baseimageurl} alt=''></img>
--- src/components/GalleryView/index.js
+++ src/components/GalleryView/index.js
@@ -1,23 +1,29 @@
-import { useParams } from "react-router-dom";
+import { useParams, Route } from "react-router-dom";
 import ArtImageTile from '../ArtImageTile';
 
 const GalleryView = ({galleries}) => {
 
 const { galleryId } = useParams();
-// console.log(galleryId)
-// console.log(galleries);
 const existingGallery = galleries.find(gallery=>gallery.id===parseInt(galleryId))
-// console.log(existingGallery);
-// if(existingGallery){
-const picArray = existingGallery.objects.map((object)=>{
-    return object.images[0]
-});
+console.log(existingGallery);
+
+// const picArray = existingGallery.objects.map((object)=>{
+//     return object.images[0]
+// });
 
     return (
         <div className="wrapper">
             <h1>Hello from GalleryView</h1>
             <h2>{existingGallery.name}</h2>
-                <ArtImageTile art={picArray} gallery={existingGallery}/>
+                {/* <ArtImageTile art={picArray} gallery={existingGallery}/> */}
+            <Route exact to={`/galleries/${galleryId}`}>
+            {existingGallery.objects.map(object=>{
+                 return (
+                     <ArtImageTile art={[object.images[0]]} gallery={existingGallery}/>
+                )
+             })}
+ {/* <ArtImageTile art={picArray} gallery={existingGallery}/> */}
+            </Route>
         </div>
     )
 // }
