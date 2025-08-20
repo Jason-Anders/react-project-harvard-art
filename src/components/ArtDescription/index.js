d51bbc5a05d149290473358105ae6fa8bc2f5f68
Jason-Anders <jason.charles.anders@gmail.com>
2025-08-20 15:26:41 -0400

    return link to gallery, gallery name displayed

--- src/components/ArtDescription/index.js
+++ src/components/ArtDescription/index.js
@@ -1,16 +1,21 @@
 
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 
-const ArtDescription = () => {
+const ArtDescription = ({gallery}) => {
 
     const {artId} = useParams();
-
+console.log(gallery);
 // check params with data for existence before using
-
+const galleryId = gallery.galleryid;
+const galleryName = gallery.name;
 // console.log(artId)
     return (
+        <div className="wrapper">
         <h1>Hi from ArtDescription #{artId}</h1>
-
+        <Link to={`/galleries/${galleryId}`}>
+        Back to Gallery: {galleryName}
+        </Link>
+</div>
     )
 };
 
