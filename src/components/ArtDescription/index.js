39903adddbbc93ac7b4ad426778c067087f135e7
Jason-Anders <jason.charles.anders@gmail.com>
2025-08-20 23:53:49 -0400

    added art details, credit, description, technique

--- src/components/ArtDescription/index.js
+++ src/components/ArtDescription/index.js
@@ -6,14 +6,29 @@ const ArtDescription = ({gallery}) => {
     const {artId } = useParams();
 
     const existingArt = gallery.objects.find(object=> object.images[0].imageid === parseInt(artId));
-
+// console.log(existingArt);
     return (
         <div className="wrapper">
-        <h1>Hi from ArtDescription #{artId}</h1>
+        <h1>{existingArt.title}</h1>
+        <p>{existingArt.description}</p>
+<h3>Credited By:</h3>
+<p>{existingArt.creditline}</p>
+<h3>Technique</h3>
+<p>{existingArt.technique}</p>
+
         <Link to={`/galleries/${gallery.id}`}>
         Back to Gallery: {gallery.name}
         </Link>
         <a href={existingArt.url}>{existingArt.title}</a>
+
+        <h2>Images</h2>
+        {existingArt.images.map(image=>{
+            return (
+                <img src={image.baseimageurl} alt="" key={existingArt.id}></img>
+            )
+        })}
+        {/* images descript credit and technique */}
+
 </div>
     )
 };
