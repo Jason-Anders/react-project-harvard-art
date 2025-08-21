fad05411a4e26080131984410c4fb246de1120ae
Jason-Anders <jason.charles.anders@gmail.com>
2025-08-21 14:38:40 -0400

    finished art museum styling and bugFixes on navigation for broken links

--- src/components/ArtDescription/index.js
+++ src/components/ArtDescription/index.js
@@ -4,18 +4,40 @@ import { useParams, Link } from "react-router-dom";
 const ArtDescription = ({gallery}) => {
 
     const {artId } = useParams();
+// console.log()
+    const existingArt = gallery.objects.find(object=> {
+        if(object.images[0]){
+    return object.images[0].imageid === parseInt(artId)
+} else {return -1}
+})
+;
+console.log(existingArt);
+// if(existingArt.images.map(img=>!img.baseimageurl)){
+//     return 'No image url'
+// }
 
-    const existingArt = gallery.objects.find(object=> object.images[0].imageid === parseInt(artId));
-// console.log(existingArt);
+// if(existingArt.images.map(img=>!img.imageid)){
+//     return 'No image id'
+// }
     return (
         <div className="wrapper">
         <h1>{existingArt.title}</h1>
         <p>{existingArt.description}</p>
 
+<p>
+        <Link to={`/galleries/${gallery.id}`}>
+        Back to Gallery: {gallery.name}
+        </Link>
+       </p>
+
+       <p>
+        <a href={existingArt.url}>More Details at HardvardArtMuseums.org</a>
+</p>
 
         <h2>Images</h2>
         {existingArt.images.map(image=>{
             return (
+
                 <img src={image.baseimageurl} alt="" key={image.imageid} className="fullImage"></img>
             )
         })}
@@ -24,15 +46,8 @@ const ArtDescription = ({gallery}) => {
 <p>{existingArt.creditline}</p>
 <h3>Technique</h3>
 <p>{existingArt.technique}</p>
+ <br></br>
 
- <p>
-        <Link to={`/galleries/${gallery.id}`}>
-        Back to Gallery: {gallery.name}
-        </Link>
-       </p>
-       <p>
-        <a href={existingArt.url}>More Details at HardvardArtMuseums.org</a>
-</p>
 
 </div>
     )
