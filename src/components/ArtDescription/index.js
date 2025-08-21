af29394122c8fc963cd62a475074d00da3e30670
Jason-Anders <jason.charles.anders@gmail.com>
2025-08-20 22:16:13 -0400

    external links to pics

--- src/components/ArtDescription/index.js
+++ src/components/ArtDescription/index.js
@@ -3,18 +3,24 @@ import { useParams, Link } from "react-router-dom";
 
 const ArtDescription = ({gallery}) => {
 
-    const {artId} = useParams();
-console.log(gallery);
+    const { artId } = useParams();
+
+    const existingGallery = gallery.objects.find(object=> object.images[0].imageid === parseInt(artId));
+//     console.log(artId);
+// console.log(gallery);
+// console.log(existingGallery);
 // check params with data for existence before using
-const galleryId = gallery.galleryid;
-const galleryName = gallery.name;
+const galleryId = existingGallery.galleryid;
+const galleryName = existingGallery.name;
 // console.log(artId)
+
     return (
         <div className="wrapper">
         <h1>Hi from ArtDescription #{artId}</h1>
         <Link to={`/galleries/${galleryId}`}>
         Back to Gallery: {galleryName}
         </Link>
+        <a href={existingGallery.url}>{existingGallery.title}</a>
 </div>
     )
 };
